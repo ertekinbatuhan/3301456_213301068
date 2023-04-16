eee966ba9528c79c621293697bb892f79d984c26
Batuhan Berk Ertekin <batuhanertekinn@gmail.com>
2023-04-17 01:22:50 +0300

    update project

---
+++ lib/view/registerView.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+
+class Register extends StatefulWidget {
+  const Register({Key? key}) : super(key: key);
+
+  @override
+  State<Register> createState() => _RegisterState();
+}
+
+class _RegisterState extends State<Register> {
+  var  formKey = GlobalKey<FormState>();
+  var userEmail = TextEditingController();
+  var userPassword = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Kayıt Olunuz"),
+
+      ),
+      body: Center(
+
+        child: Column(
+          children: [
+            Image.asset("icons/register.png",width: 300,),
+
+            Form(key: formKey,
+            child: Column(
+              children: [
+
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: TextFormField(
+                    controller: userEmail,
+                    decoration: const InputDecoration(
+                      hintText: "E posta giriniz" ,
+                      prefixIcon: Icon(Icons.account_box),
+                        border: OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(25.0)))),
+                    ),
+                ),
+
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: TextFormField(
+                    controller: userPassword,
+                    decoration: const InputDecoration(
+                      hintText: "Şifre giriniz" ,
+                      prefixIcon: Icon(Icons.lock),
+                        border: OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(25.0)))),
+                    ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 10.0),
+                  child: SizedBox(width: 150,
+                    child: ElevatedButton(
+                      child:  Text("Kayıt Ol"),
+                      style: ElevatedButton.styleFrom(
+                          shape: const RoundedRectangleBorder(
+                              borderRadius: BorderRadius.all(Radius.circular(10.0))
+                          )
+                      ),
+
+
+
+                      onPressed: () {
+
+                      },
+                    ),
+                  ),
+                )
+
+
+              ],
+            ),),
+
+
+
+          ],
+        ),
+      ),
+    );
+
+  }
+}
